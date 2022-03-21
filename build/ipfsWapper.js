ae0fb8410a0ce4092746dbdedefc6a253c79761c
b-rad-c <bradcorlett@gmail.com>
2022-03-20 21:54:41 -0700

    add ipfs functions

---
+++ build/ipfsWapper.js
@@ -0,0 +1,15 @@
+import { create } from 'ipfs-http-client';
+import { Article } from './article';
+export function testIPFSConnection(host) {
+  return create(host).version();
+}
+export async function loadArticleFromIPFS(client, path) {
+  let result = '';
+  let utf8decoder = new TextDecoder();
+
+  for await (const chunk of client.files.read(path)) {
+    result += utf8decoder.decode(chunk);
+  }
+
+  return Article.fromJSONString(result);
+}
